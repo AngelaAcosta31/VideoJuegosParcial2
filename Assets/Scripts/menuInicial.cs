2a0c1caed46d508ccd5b2a0f0b871af941d01be1
Angela Acosta <71620891+AngelaAcosta31@users.noreply.github.com>
2024-04-23 20:18:03 -0500

    Creacion Menu Principal

---
+++ Assets/Scripts/menuInicial.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class menuInicial : MonoBehaviour
+{
+    //public AudioSource clip;
+    public void Jugar(){
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Juego");
+
+    }
+
+    public void Instrucciones(){
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Instrucciones");
+    }
+    public void Creditos(){
+        //SceneManager.UnloadSceneAsync("menuInicial");
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void Salir(){
+        Application.Quit();
+    }
+
+    /*
+    public void PlaySoundBtn(AudioClip audio){
+        clip.PlayOneShot(audio);
+    }
+    */
+}
